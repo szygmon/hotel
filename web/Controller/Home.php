8a2c92ae66ce01ad0808546b6fa0beaa24be9cce
szygmon <szygmon@hotmail.com>
2016-03-15 13:55:56 +0100

    second :D

--- web/Controller/Home.php
+++ web/Controller/Home.php
@@ -6,99 +6,145 @@ use Core\Response;
 
 class Home {
 
-	/** @var \User\Me */
-	protected $me;
+    /** @var \User\Me */
+    protected $me;
 
-	/** @var \Doctrine\ORM\EntityManager */
-	protected $em;
+    /** @var \Doctrine\ORM\EntityManager */
+    protected $em;
 
-	/** @var \HomeUtil */
-	protected $homeUtil;
+    /** @var \HomeUtil */
+    protected $homeUtil;
 
-	/**
-	 * @param \Doctrine\ORM\EntityManager $em
-	 * @param \HomeUtil $HomeUtil
-	 */
-	function __construct($em, $HomeUtil) {
-		$this->em = $em;
-		$this->homeUtil = $HomeUtil;
-	}
+    /**
+     * @param \Doctrine\ORM\EntityManager $em
+     * @param \HomeUtil $HomeUtil
+     */
+    function __construct($em, $HomeUtil) {
+        $this->em = $em;
+        $this->homeUtil = $HomeUtil;
+    }
 
-	/**
-	 * Index Site
-	 * @Route()
-	 * @param \User\Me $Me
-	 * @param \Core\Router $Router
-	 */
-	public function index($Me, $Router) {
-		if ($Me->auth('student'))
-			$Router->redirect('Student/index');
+    /**
+     * Index Site
+     * @Route()
+     * @param \User\Me $Me
+     * @param \Core\Router $Router
+     */
+    public function index($Me, $Router) {
+        (new \UserManager)->switchSchema('hotel');
+        if ($Me->auth('student'))
+            $Router->redirect('Student/index');
 
-		if ($Me->auth('user'))
-			return new Response([], 'Home/indexSchool');
+        if ($Me->auth('user'))
+            return new Response([], 'Home/indexSchool');
 
-		return array("salesPage" => !$Router->getSubdomain());
-	}
+        return array("salesPage" => !$Router->getSubdomain());
+    }
 
-        /**
-	 * Pokoje
-	 * @Route(/rooms)
-	 */
-	public function rooms() {
-		
-		return array("salesPage" => true);
-	}
-        
-	/**
-	 * Register
-	 * @Route(/register)
-	 */
-	public function register() {
-		if (isset($_POST['username']))
-			$msg = $this->homeUtil->registerForm($_POST);
-		return array("salesPage" => true);
-	}
+    /**
+     * Pokoje
+     * @Route(/rooms)
+     */
+    public function rooms() {
 
-	/**
-	 * @Route(/removeNotif)
-	 */
-	public function removeNotif() {
-		$n = $this->em->getRepository('\Model\\Notification')->find($_POST['id']);
-		$this->em->remove($n);
-		$this->em->flush();
+        return array("salesPage" => true);
+    }
 
-		return array("info" => "brak");
-	}
+    /**
+     * Pokoje
+     * @Route(/signin)
+     */
+    public function signin() {
 
-	/**
-	 * Login site
-	 * @Route(/login)
-	 * @param \User\Me $Me
-	 * @param \Core\Router $Router
-	 */
-	public function login($Me, $Router) {
-		$schools = $this->em->getRepository('Model\\School')->findAll();
-		$s = array();
-		foreach ($schools as $school) {
-			$s[$school->getAlias()] = $school->getName();
-		}
+        return array("salesPage" => true);
+    }
 
-		$msg = null;
-		if (isset($_POST['username']))
-			$msg = $this->homeUtil->loginForm($_POST);
+    /**
+     * Pokoje
+     * @Route(/offert)
+     */
+    public function offert() {
 
-		return array("salesPage" => !$Router->getSubdomain(), 'schools' => $s, 'msg' => $msg);
-	}
+        return array("salesPage" => true);
+    }
 
-	/**
-	 * Logout
-	 * @Route("/logout")
-	 * @param \User\Me $Me
-	 * @param \Core\Router $Router
-	 */
-	public function logout($Me, $Router) {
-		$Me->logout();
-		$Router->redirect('Home/index');
-	}
+    /**
+     * Typografia - chwilowo
+     * @Route(/tp)
+     */
+    public function tp() {
+
+        return array("salesPage" => true);
+    }
+
+    /**
+     * Pokoje
+     * @Route(/details)
+     */
+    public function details() {
+
+        return array("salesPage" => true);
+    }
+
+    /**
+     * Pokoje
+     * @Route(/contact)
+     */
+    public function contact() {
+
+        return array("salesPage" => true);
+    }
+
+    /**
+     * Register
+     * @Route(/register)
+     */
+    public function register() {
+        if (isset($_POST['username']))
+            $msg = $this->homeUtil->registerForm($_POST);
+        return array("salesPage" => true);
+    }
+
+    /**
+     * @Route(/removeNotif)
+     */
+    public function removeNotif() {
+        $n = $this->em->getRepository('\Model\\Notification')->find($_POST['id']);
+        $this->em->remove($n);
+        $this->em->flush();
+
+        return array("info" => "brak");
+    }
+
+    /**
+     * Login site
+     * @Route(/admin)
+     * @param \User\Me $Me
+     * @param \Core\Router $Router
+     */
+    public function admin($Me, $Router) {
+        $schools = $this->em->getRepository('Model\\School')->findAll();
+        $s = array();
+        foreach ($schools as $school) {
+            $s[$school->getAlias()] = $school->getName();
+        }
+
+        $msg = null;
+        if (isset($_POST['username']))
+            $msg = $this->homeUtil->loginForm($_POST);
+
+        return array("salesPage" => !$Router->getSubdomain(), 'schools' => $s, 'msg' => $msg);
+    }
+
+    /**
+     * Logout
+     * @Route("/logout")
+     * @param \User\Me $Me
+     * @param \Core\Router $Router
+     */
+    public function logout($Me, $Router) {
+        $Me->logout();
+        $Router->redirect('Home/index');
+    }
 
 }
