319132f9b33861bfdc12ac7b2d421552f4be0729
szygmon <szygmon@hotmail.com>
2016-04-14 00:06:26 +0200

    kalendarz po polsku, wywalone niepotrzebne f-cje

--- web/View/js/jquery-ui.js
+++ web/View/js/jquery-ui.js
@@ -7251,19 +7251,19 @@ function Datepicker() {
 	this._dayOverClass = 'ui-datepicker-days-cell-over'; // The name of the day hover marker class
 	this.regional = []; // Available regional settings, indexed by language code
 	this.regional[''] = { // Default regional settings
-		closeText: 'Done', // Display text for close link
-		prevText: 'Prev', // Display text for previous month link
-		nextText: 'Next', // Display text for next month link
-		currentText: 'Today', // Display text for current month link
-		monthNames: ['January','February','March','April','May','June',
-			'July','August','September','October','November','December'], // Names of months for drop-down and formatting
-		monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], // For formatting
+		closeText: 'Zamknij', // Display text for close link
+		prevText: 'Wstecz', // Display text for previous month link
+		nextText: 'Dalej', // Display text for next month link
+		currentText: 'Dziś', // Display text for current month link
+		monthNames: ['Styczeń','Luty','Marrzec','Kwiecień','Maj','Czerwiec',
+			'Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień'], // Names of months for drop-down and formatting
+		monthNamesShort: ['Sty', 'Lut', 'Mar', 'Kwi', 'Maj', 'Cze', 'Lip', 'Sie', 'Wrz', 'Paź', 'Lis', 'Gru'], // For formatting
 		dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], // For formatting
-		dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'], // For formatting
-		dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'], // Column headings for days starting at Sunday
+		dayNamesShort: ['Nie', 'Pon', 'Wto', 'Śro', 'Czw', 'Pią', 'Sob'], // For formatting
+		dayNamesMin: ['Nd','Pn','Wt','Śr','Cz','Pt','So'], // Column headings for days starting at Sunday
 		weekHeader: 'Wk', // Column header for week of the year
 		dateFormat: 'yy-mm-dd', // See format options on parseDate
-		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
+		firstDay: 1, // The first day of the week, Sun = 0, Mon = 1, ...
 		isRTL: false, // True if right-to-left language, false if left-to-right
 		showMonthAfterYear: false, // True if the year select precedes month, false for month then year
 		yearSuffix: '' // Additional text to append to the year in the month headers
